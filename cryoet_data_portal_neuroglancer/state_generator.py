75bca5b081e323b4954e45e7acf0f193b3f267df
Sean Martin <martins7@tcd.ie>
2024-06-06 13:41:02 +0200

    fix: correct check for image layers in state combine

--- cryoet_data_portal_neuroglancer/state_generator.py
+++ cryoet_data_portal_neuroglancer/state_generator.py
@@ -142,7 +142,9 @@ def combine_json_layers(
         "crossSectionBackgroundColor": "#000000",
         "layout": "4panel",
     }
-    if image_layers is not None:
+    # TODO (skm) the second check can probably be removed once VR
+    # layers have been fully decided upon
+    if len(image_layers) > 0 and "_position" in image_layers[0]:
         combined_json["position"] = image_layers[0]["_position"]
         combined_json["crossSectionScale"] = image_layers[0]["_crossSectionScale"]
         combined_json["projectionScale"] = image_layers[0]["_projectionScale"]
