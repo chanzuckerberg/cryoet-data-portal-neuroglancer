3163ebf33f327b5dea3c0ff49efd3bd676f24402
Sean Martin <martins7@tcd.ie>
2024-06-24 18:43:34 +0200

    feat: create a point annotation shader

--- cryoet_data_portal_neuroglancer/models/shader_builder.py
+++ cryoet_data_portal_neuroglancer/models/shader_builder.py
@@ -36,6 +36,9 @@ class ShaderBuilder:
             "shaderControls": self._shader_controls,
         }
 
+    def sort_shader_preamble(self, sorting: lambda x: x):
+        self._shader_pre_main = "\n".join(sorted(self._shader_pre_main.split("\n"), key=sorting))
+
     def make_invlerp_component(
         self,
         name: str,
@@ -74,22 +77,86 @@ class ShaderBuilder:
         return f"#uicontrol vec3 {name} color"
 
 
-class OrientedPointShaderBuilder(ShaderBuilder):
-    def __init__(self, point_size_multiplier: float = 1.0, line_width: float = 1.0):
+class PointShaderBuilder(ShaderBuilder):
+    def __init__(
+        self,
+        point_size_multiplier: float = 1.0,
+        is_instance_segmentation: bool = False,
+        color: str = "#FFFFFF",
+    ):
         super().__init__()
         self.point_size_multiplier = point_size_multiplier
+        self.is_instance_segmentation = is_instance_segmentation
+        self.color = color
+
+    def _make_default_shader(self):
+        self.add_to_shader_controls(
+            self.make_slider_component(
+                "pointScale",
+                min_value=0.01,
+                max_value=2.0,
+                default_value=self.point_size_multiplier,
+            ),
+        )
+        self.add_to_shader_controls(
+            self.make_slider_component("opacity", min_value=0.0, max_value=1.0, default_value=1.0),
+        )
+        if not self.is_instance_segmentation:
+            self.add_to_shader_controls(self.make_color_component("color", self.color))
+
+    def _get_color_setter(self):
+        return "color" if not self.is_instance_segmentation else "prop_color()"
+
+
+class NonOrientedPointShaderBuilder(PointShaderBuilder):
+
+    def __init__(
+        self,
+        point_size_multiplier: float = 1.0,
+        is_instance_segmentation: bool = False,
+        color: str = "#FFFFFF",
+    ):
+        super().__init__(
+            point_size_multiplier=point_size_multiplier,
+            is_instance_segmentation=is_instance_segmentation,
+            color=color,
+        )
+        self._make_default_shader()
+
+    def _make_default_shader(self):
+        super()._make_default_shader()
+
+        self.add_to_shader_main(
+            (
+                [
+                    f"setColor(vec4({self._get_color_setter()}, opacity));",
+                    "setPointMarkerSize(pointScale * prop_diameter());",
+                    "setPointMarkerBorderWidth(0.1);",
+                ]
+            ),
+        )
+
+
+class OrientedPointShaderBuilder(PointShaderBuilder):
+    def __init__(
+        self,
+        point_size_multiplier: float = 1.0,
+        is_instance_segmentation: bool = False,
+        color: str = "#FFFFFF",
+        line_width: float = 1.0,
+    ):
+        super().__init__(
+            point_size_multiplier=point_size_multiplier,
+            is_instance_segmentation=is_instance_segmentation,
+            color=color,
+        )
         self.line_width = line_width
         self._make_default_shader()
 
     def _make_default_shader(self):
+        super()._make_default_shader()
         self.add_to_shader_controls(
             (
-                self.make_slider_component(
-                    "pointScale",
-                    min_value=0.01,
-                    max_value=2.0,
-                    default_value=self.point_size_multiplier,
-                ),
                 self.make_slider_component(
                     "lineWidth",
                     min_value=0.01,
@@ -101,10 +168,25 @@ class OrientedPointShaderBuilder(ShaderBuilder):
         self.add_to_shader_main(
             (
                 "setLineWidth(lineWidth);",
-                "setLineColor(prop_line_color());",
+                "setLineColor(vec4(prop_line_color(), opacity));",
                 "setEndpointMarkerSize(pointScale * prop_diameter(), pointScale * 0.5 * prop_diameter());",
-                "setEndpointMarkerColor(prop_point_color());",
+                f"setEndpointMarkerColor(vec4({self._get_color_setter()}, opacity));",
                 "setEndpointMarkerBorderWidth(0.1);",
-                "setEndpointMarkerBorderColor(vec3(0.0, 0.0, 0.0));",
+                "setEndpointMarkerBorderColor(vec4(0.0, 0.0, 0.0, opacity));",
             ),
         )
+        # Sort the shader preamble to ensure that the sliders are before the color
+        mapping = {
+            "pointScale": 0,
+            "lineWidth": 1,
+            "opacity": 2,
+            "color": 3,
+        }
+
+        def sorting(x):
+            for m in mapping:
+                if m in x:
+                    return mapping[m]
+            return len(mapping)
+
+        self.sort_shader_preamble(sorting)
