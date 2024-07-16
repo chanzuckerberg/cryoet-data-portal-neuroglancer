8cf3f06f9123f6c12214029ef7dccf9d8d9e4777
Sean Martin <martins7@tcd.ie>
2024-07-16 16:51:49 +0200

    refactor: bring LOD into mesh encdoing

--- cryoet_data_portal_neuroglancer/precompute/instance_mesh.py
+++ cryoet_data_portal_neuroglancer/precompute/instance_mesh.py
@@ -1,16 +1,18 @@
 from typing import Any
 
 import numpy as np
-import tqdm
 import trimesh
+from tqdm import tqdm
 
+from cryoet_data_portal_neuroglancer.precompute.glb_meshes import decimate_mesh
 from cryoet_data_portal_neuroglancer.utils import rotate_and_translate_mesh
 
 
 def encode_oriented_mesh(
     input_geometry: trimesh.Scene | trimesh.Trimesh,
     data: list[dict[str, Any]],
-):
+    num_lods: int = 1,
+) -> list[trimesh.Scene]:
     """Turn a mesh into an oriented mesh with a list of orientations and translations
 
     Parameters
@@ -19,6 +21,13 @@ def encode_oriented_mesh(
         The scene containing the mesh or the mesh itself
     data : list[dict[str, Any]]
         The list of orientations and translations
+    num_lods : int, optional
+        The number of levels of detail to generate, by default 1
+
+    Returns
+    -------
+    list[trimesh.Scene]
+        The list of scenes containing the oriented meshes
     """
     if isinstance(input_geometry, trimesh.Trimesh):
         mesh = input_geometry
@@ -37,14 +46,16 @@ def encode_oriented_mesh(
     # is 1.0 nm, and then we scale the mesh to the real resolution
     # of the tomogram inside of the neuroglancer viewer
     # instead of at the time of encoding the mesh
-    new_scene = trimesh.Scene()
-    for index, point in tqdm.tqdm(
-        enumerate(data),
-        total=len(data),
-        desc="Rotating and Translating Instanced Mesh",
-    ):
-        translation = np.array([point["location"][k] for k in ("x", "y", "z")])
-        rotation = np.array(point["xyz_rotation_matrix"])
-        rotate_and_translate_mesh(scaled, new_scene, index, rotation, translation)
 
-    return new_scene
+    decimated_meshes = decimate_mesh(scaled, num_lods, as_trimesh=True)
+
+    results = []
+    for mesh in tqdm(decimated_meshes, desc="Processing LODs"):
+        new_scene = trimesh.Scene()
+        for index, point in enumerate(data):
+            translation = np.array([point["location"][k] for k in ("x", "y", "z")])
+            rotation = np.array(point["xyz_rotation_matrix"])
+            rotate_and_translate_mesh(mesh, new_scene, index, rotation, translation)
+        results.append(new_scene)
+
+    return results
