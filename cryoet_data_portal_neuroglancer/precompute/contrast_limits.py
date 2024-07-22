15215c42a9d5a8825afb8d148f51af6b418ab6c3
Sean Martin <martins7@tcd.ie>
2024-07-22 18:05:49 +0200

    feat: attempt to auto find end of meaningful Z-data

--- cryoet_data_portal_neuroglancer/precompute/contrast_limits.py
+++ cryoet_data_portal_neuroglancer/precompute/contrast_limits.py
@@ -4,12 +4,13 @@ from abc import abstractmethod
 from typing import Optional
 
 import numpy as np
+from scipy.signal import find_peaks
 
 
 def _restrict_volume_around_central_z_slice(
     volume: "np.ndarray",
     central_z_slice: Optional[int] = None,
-    z_radius: int = 5,
+    z_radius: Optional[int] = None,
 ) -> "np.ndarray":
     """Restrict a 3D volume to a region around a central z-slice.
 
@@ -22,16 +23,32 @@ def _restrict_volume_around_central_z_slice(
             By default None, in which case the central z-slice is the middle slice.
         z_radius: int, optional.
             The number of z-slices to include above and below the central z-slice.
-            By default 5.
+            By default None, in which case it is auto computed.
 
     Returns
     -------
         np.ndarray
             3D numpy array. The restricted volume.
     """
+    standard_deviation_per_z_slice = np.std(volume, axis=(1, 2))
     central_z_slice = central_z_slice or (0.5 * volume.shape[0] - 0.5)
-    z_min = max(0, int(np.ceil(central_z_slice - z_radius)))
-    z_max = min(volume.shape[0], int(np.floor(central_z_slice + z_radius) + 1))
+
+    if z_radius is None:
+        lowest_points = find_peaks(-standard_deviation_per_z_slice, prominence=0.1)[0]
+        if len(lowest_points) < 2:
+            # TODO create fallback instead
+            raise ValueError("Not enough low points found")
+        for value in lowest_points:
+            if value < central_z_slice:
+                z_min = value
+            else:
+                z_max = min(volume.shape[0], value + 1)
+                break
+
+    else:
+        z_min = max(0, int(np.ceil(central_z_slice - z_radius)))
+        z_max = min(volume.shape[0], int(np.floor(central_z_slice + z_radius) + 1))
+    print(f"Z min: {z_min}, Z max: {z_max}")
     return volume[z_min:z_max]
 
 
@@ -51,7 +68,7 @@ class ContrastLimitCalculator:
         self,
         volume: "np.ndarray",
         central_z_slice: Optional[int] = None,
-        z_radius: int = 5,
+        z_radius: Optional[int] = None,
     ) -> None:
         """Set the volume and z-limits for calculating contrast limits.
 
@@ -62,9 +79,9 @@ class ContrastLimitCalculator:
             central_z_slice: int or None, optional.
                 The central z-slice around which to restrict the volume.
                 By default None, in which case the central z-slice is the middle slice.
-            z_radius: int, optional.
+            z_radius: int or None, optional.
                 The number of z-slices to include above and below the central z-slice.
-                By default 5.
+                By default None.
         """
         self.volume = volume
         self.trim_volume_around_central_zslice(central_z_slice, z_radius)
@@ -72,7 +89,7 @@ class ContrastLimitCalculator:
     def trim_volume_around_central_zslice(
         self,
         central_z_slice: Optional[int] = None,
-        z_radius: int = 5,
+        z_radius: Optional[int] = None,
     ) -> None:
         """Trim the volume around a central z-slice.
 
@@ -81,9 +98,9 @@ class ContrastLimitCalculator:
             central_z_slice: int or None, optional.
                 The central z-slice around which to restrict the volume.
                 By default None, in which case the central z-slice is the middle slice.
-            z_radius: int, optional.
+            z_radius: int or None, optional.
                 The number of z-slices to include above and below the central z-slice.
-                By default 5.
+                By default None, in which case it is auto computed.
         """
         self.volume = _restrict_volume_around_central_z_slice(
             self.volume,
