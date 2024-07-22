0376186b4393a8559d2a98f3093c0c71b1cdbde8
Sean Martin <martins7@tcd.ie>
2024-07-22 13:45:57 +0200

    feat: allow specify resolution level in dask load

--- cryoet_data_portal_neuroglancer/io.py
+++ cryoet_data_portal_neuroglancer/io.py
@@ -3,13 +3,22 @@ from ome_zarr.io import parse_url
 from ome_zarr.reader import Reader
 
 
-def load_omezarr_data(input_filepath: str) -> da.Array:
-    """Load the OME-Zarr data and return a dask array"""
+def load_omezarr_data(input_filepath: str, resolution_level: int = 0) -> da.Array:
+    """Load the OME-Zarr data and return a dask array
+
+    Parameters
+    ----------
+        input_filepath: str
+            Path to the OME-Zarr file.
+        resolution_level: int, optional
+            Resolution level to load.
+            By default 0 - the highest resolution.
+    """
     url = parse_url(input_filepath)
     if not url:
         raise ValueError(f"Input file {input_filepath} is not a ZARR file")
     reader = Reader(url)
     nodes = list(reader())
     image_node = nodes[0]
-    dask_data = image_node.data[0]
+    dask_data = image_node.data[resolution_level]
     return dask_data.persist()
