54fe625a70d66c2340d91959ed414c3a37386885
Sean Martin <martins7@tcd.ie>
2024-08-15 13:14:02 +0200

    test: add mesh from segmentation test

--- tests/test_mesh.py
+++ tests/test_mesh.py
@@ -1,7 +1,13 @@
+import os
+
 import cloudvolume
+import numpy as np
 import pytest
 import trimesh
+import zarr
 from cloudvolume import CloudVolume
+from ome_zarr.io import parse_url
+from ome_zarr.writer import write_image
 
 from cryoet_data_portal_neuroglancer.precompute.mesh import (
     decimate_mesh,
@@ -10,6 +16,7 @@ from cryoet_data_portal_neuroglancer.precompute.mesh import (
     generate_multilabel_multiresolution_mesh,
     generate_multiresolution_mesh,
 )
+from cryoet_data_portal_neuroglancer.precompute.segmentation_mask import encode_segmentation
 
 
 @pytest.mark.parametrize(
@@ -28,16 +35,17 @@ def test_determine_chunk_size_for_lod(mesh_shape, max_lod, min_chunk_dim, expect
     assert num_lods == expected_lods
 
 
-def generic_mesh_checks(tmp_path):
+def generic_mesh_checks(tmp_path, num_files=2):
     # There should be a mesh folder and an info file
     assert (tmp_path / "mesh").is_dir()
     assert (tmp_path / "info").is_file()
     # Nothing else should be present in the output directory
-    assert len(list(tmp_path.iterdir())) == 2
+    assert len(list(tmp_path.iterdir())) == num_files
 
     # In the mesh folder, there should be also an info file
-    # And one shard file
     assert (tmp_path / "mesh" / "info").is_file()
+
+    # There should either be one shard file
     assert (tmp_path / "mesh" / "0.shard").is_file()
     # Nothing else should be present in the mesh directory
     assert len(list((tmp_path / "mesh").iterdir())) == 2
@@ -81,3 +89,49 @@ def test_generate_multilabel_multiresolution_mesh(tmp_path):
         bounding_box_size=[100, 100, 120],
     )
     generic_mesh_checks(tmp_path)
+
+
+@pytest.mark.parametrize(
+    "segmentation_shape",
+    [(128, 128, 128), (120, 53, 12), (40, 20, 25)],
+)
+def test_mesh_from_segmentation(segmentation_shape, tmp_path):
+    zarr_path = tmp_path / "segmentation.zarr"
+    output_path = tmp_path / "converted"
+
+    # Generate OME-zarr data and write it to tmp_path
+    segmentation_shape = segmentation_shape[::-1]  # Z, Y, X
+    data = np.zeros(segmentation_shape, dtype=np.uint8)
+    data[:, :10, :] = 1
+    data[:, :, :10] = 2
+
+    # write the image data
+    if not zarr_path.exists():
+        store = parse_url(zarr_path, mode="w").store
+        root = zarr.group(store=store)
+        write_image(image=data, group=root, axes="zyx")
+
+    encode_segmentation(
+        zarr_path,
+        output_path,
+        resolution=[1, 1, 1],
+        include_mesh=True,
+        convert_non_zero_to=1,
+        max_lod=2,
+        delete_existing=True,
+    )
+
+    # A segmentation should be created
+    assert (output_path / "data").is_dir()
+
+    # Print the contents of the mesh directory
+    print(os.listdir(output_path / "mesh"))
+
+    # A mesh shouuld be created
+    #generic_mesh_checks(output_path, num_files=4)
+
+
+if __name__ == "__main__":
+    from pathlib import Path
+
+    test_mesh_from_segmentation((120, 53, 12), Path("./tmp"))
